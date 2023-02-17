155a0b5caa772cdbd554dcfbd9524b9e8e157665
raviteja <ravitejatfcdemo@gmail.com>
2023-02-17 18:14:00 +0530

    Terraform

--- main.tf
+++ main.tf
@@ -27,7 +27,6 @@ resource "aws_security_group" "docdb" {
     to_port          = 0
     protocol         = "-1"
     cidr_blocks      = ["0.0.0.0/0"]
-    ipv6_cidr_blocks = ["::/0"]
   }
 
   tags = merge(
@@ -35,3 +34,22 @@ resource "aws_security_group" "docdb" {
     { Name = "${var.env}-docdb-security-group" }
   )
 }
+
+
+resource "aws_docdb_cluster" "docdb" {
+  cluster_identifier      = "${var.env}-docdb-cluster"
+  engine                  = "docdb"
+  master_username         = "foo"
+  master_password         = "mustbeeightchars"
+  skip_final_snapshot     = true
+  db_subnet_group_name    = aws_docdb_subnet_group.default.name
+  vpc_security_group_ids  = aws_security_group.docdb.id
+
+  tags = merge(
+    local.common_tags,
+    { Name = "${var.env}-docdb-cluster" }
+  )
+}
+
+# skip_final_snapshot in organization must be false, it takes backup before delete
+# pickup username and password from parameter store
