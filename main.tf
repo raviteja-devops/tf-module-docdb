b6125b2a9d9212d29d73e0a71010f07d7be311d5
raviteja <ravitejatfcdemo@gmail.com>
2023-02-17 16:49:01 +0530

    Terraform

--- main.tf
+++ main.tf
@@ -7,3 +7,31 @@ resource "aws_docdb_subnet_group" "default" {
     { Name = "${var.env}-docdb-subnet-group" }
   )
 }
+
+
+resource "aws_security_group" "docdb" {
+  name        = "${var.env}-docdb-security-group"
+  description = "${var.env}-docdb-security-group"
+  vpc_id      = var.vpc_id
+
+  ingress {
+    description      = "MongoDB"
+    from_port        = 27017
+    to_port          = 27017
+    protocol         = "tcp"
+    cidr_blocks      = var.allow_cidr
+  }
+
+  egress {
+    from_port        = 0
+    to_port          = 0
+    protocol         = "-1"
+    cidr_blocks      = ["0.0.0.0/0"]
+    ipv6_cidr_blocks = ["::/0"]
+  }
+
+  tags = merge(
+    local.common_tags,
+    { Name = "${var.env}-docdb-security-group" }
+  )
+}
