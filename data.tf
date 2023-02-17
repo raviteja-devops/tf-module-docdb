8a890e51782d6b106a01d19749f9f4ae5f59c308
raviteja <ravitejatfcdemo@gmail.com>
2023-02-17 18:33:34 +0530

    Terraform

--- data.tf
+++ data.tf
@@ -1,7 +1,7 @@
 data "aws_ssm_parameter" "DB_ADMIN_USER" {
-  name = "DB_ADMIN_USER"
+  name = "${var.env}.docdb.DB_ADMIN_USER"
 }
 
 data "aws_ssm_parameter" "DB_ADMIN_PASS" {
-  name = "DB_ADMIN_PASS"
+  name = "${var.env}.docdb.DB_ADMIN_PASS"
 }
