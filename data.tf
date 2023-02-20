a874c3dbf223bf114e93a9d0ab80e64d0052aacb
raviteja <ravitejatfcdemo@gmail.com>
2023-02-20 10:26:13 +0530

    Terraform

--- data.tf
+++ data.tf
@@ -6,6 +6,6 @@ data "aws_ssm_parameter" "DB_ADMIN_PASS" {
   name = "${var.env}.docdb.DB_ADMIN_PASS"
 }
 
-data "aws_kms_key" "key" {
-  key_id = "alias/roboshop"
-}
+#data "aws_kms_key" "key" {
+#  key_id = "alias/roboshop"
+#}
