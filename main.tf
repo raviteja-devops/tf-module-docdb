4173c4f16a1350e7db4bce55ac13b80a9d2cde61
raviteja <ravitejatfcdemo@gmail.com>
2023-02-20 12:02:45 +0530

    Terraform

--- main.tf
+++ main.tf
@@ -69,8 +69,8 @@ resource "aws_docdb_cluster_instance" "cluster_instances" {
   identifier         = "${var.env}-docdb-cluster-instances-${count.index + 1}"
   cluster_identifier = aws_docdb_cluster.docdb.id
   instance_class     = var.instance_class
-  storage_encrypted       = true
-  kms_key_id              = data.aws_kms_key.key.arn
+#  storage_encrypted       = true
+#  kms_key_id              = data.aws_kms_key.key.arn
   tags = merge(
     local.common_tags,
     { Name = "${var.env}-docdb-cluster-instances-${count.index + 1}" }
